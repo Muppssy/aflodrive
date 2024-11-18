44253b0a047f9eee18afa3557c6b407db9b3e09e
muppssy <muppssy@gmail.com>
2024-11-18 10:34:56 +0100

    projet bootcamp aflodrive

---
+++ Back/codepromo.php
@@ -0,0 +1,21 @@
+<?php
+require_once "database.php";
+$db = new Database();
+$GLOBALS['database'] = $db->etatConnexion();
+
+$codepromo = $_POST["codepromo"];
+$prix_promo = $_POST["prixtotal"];
+$requete = "SELECT pourcentage FROM code_promo where nom_promo = '".$codepromo."'";
+error_log($requete);
+$result = mysqli_query($GLOBALS['database'], $requete) or die;
+          if ($data = mysqli_fetch_assoc($result)) {
+
+            $promo  = $data['pourcentage']."%";
+            $prix_promo = $_POST["prixtotal"]- ($_POST["prixtotal"]*$data['pourcentage']/100);
+            
+        }else{
+            $promo  = "votre code n'est pas valide";
+        }
+    echo json_encode(array("promo"=>$promo, "prix_promo"=>$prix_promo));
+
+?>
